dad5039053e42bd604b3ae21ef12dadfdd297874
andela-sjames <samuel.james@andela.com>
2018-05-13 00:24:59 +0100

    use proper representaion for model on the admin

--- noteapp/summary/models.py
+++ noteapp/summary/models.py
@@ -6,5 +6,8 @@ class SummaryNote(models.Model):
     title = models.CharField(max_length=60)
     content = models.TextField()
 
-    def __unicode__(self):
+    def __str__ (self):
         return self.title
+
+    def __repr__ (self):
+        return '<SummaryNote %s>' % self.title
