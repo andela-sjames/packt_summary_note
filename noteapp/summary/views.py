bc257d9e57d442303679d140e3a91c33e6ee5a41
andela-sjames <samuel.james@andela.com>
2018-05-12 15:52:24 +0100

    add homeview

--- noteapp/summary/views.py
+++ noteapp/summary/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'all_notes.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
