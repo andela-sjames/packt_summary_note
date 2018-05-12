96c6212c5fb9f33ccd4d3aa401746f93068831d4
andela-sjames <samuel.james@andela.com>
2018-05-12 16:20:08 +0100

    add summary note model

--- noteapp/summary/models.py
+++ noteapp/summary/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class SummaryNote(models.Model):
+    title = models.CharField(max_length=60)
+    content = models.models.TextField()
+
+     def __unicode__(self):
+        return self.title
