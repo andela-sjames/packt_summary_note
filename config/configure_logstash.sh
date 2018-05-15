6101152174540de19014f0177da971acf1bbe79e
andela-sjames <samuel.james@andela.com>
2018-05-15 21:44:52 +0100

    remove redundant config folder

--- config/configure_logstash.sh
+++
@@ -1,6 +0,0 @@
-chown -R logstash:root /etc/logstash/conf.d
-chmod 0750 /etc/logstash/conf.d
-chmod 0640 /etc/logstash/conf.d/*
-
-logstash -f /etc/logstash/conf.d/logstash.conf
-bin/logstash-plugin install logstash-filter-translate
